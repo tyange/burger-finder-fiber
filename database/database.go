1c1bc640a0f59bd0fc6a795aba7800fca72ee1a2
tyange <usun16@gmail.com>
2022-08-21 20:10:39 +0900

    mysql db 연결 설정

---
+++ database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"burger-finder-fiber/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var (
+	DBConn *gorm.DB
+)
+
+func ConnectDb() {
+	dsn := "root:tyange12@tcp(127.0.0.1:3306)/gin_tutorial?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.AutoMigrate(&models.Ingredient{})
+	DBConn = db
+}
