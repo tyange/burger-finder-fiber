eb28c3ebda6181fc5b17081a39c3bea4ec68b88d
tyange <usun16@gmail.com>
2022-08-21 23:07:46 +0900

    AllIngredients & AddIngredient

--- app.go
+++ app.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	_ "log"
+
 	"burger-finder-fiber/database"
+	"burger-finder-fiber/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+func setUpRoutes(app *fiber.App) {
+	app.Get("/", routes.AllIngredients)
+	app.Post("/", routes.AddIngredient)
+}
 
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
+	setUpRoutes(app)
+
 	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
 	})
 
 	app.Listen(":3000")
---
+++ routes/rotues.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"burger-finder-fiber/database"
+	"burger-finder-fiber/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AllIngredients(c *fiber.Ctx) error {
+	var ingredients []models.Ingredient
+
+	database.DBConn.Find(&ingredients)
+
+	return c.Status(200).JSON(ingredients)
+}
+
+func AddIngredient(c *fiber.Ctx) error {
+	ingredient := new(models.Ingredient)
+	if err := c.BodyParser(ingredient); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.DBConn.Create(&ingredient)
+
+	return c.Status(200).JSON(ingredient)
+}
