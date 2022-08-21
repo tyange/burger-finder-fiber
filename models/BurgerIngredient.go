c31e5da5bcf270269a99794d17a21a73eb1da455
tyange <usun16@gmail.com>
2022-08-21 20:10:06 +0900

    burger-ingredient 모델 작성

---
+++ models/BurgerIngredient.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type BurgerIngredients struct {
+	gorm.Model
+
+	Id             uint `gorm:"primary_key"`
+	BurgerId       int
+	IngredientId   int
+	IngredientName string
+	Amount         int
+}
