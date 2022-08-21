ab16573d484f82c6235c14feef3ed6f9887590f8
tyange <usun16@gmail.com>
2022-08-21 20:10:58 +0900

    mysql db 연결 설정

--- app.go
+++ app.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"burger-finder-fiber/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
